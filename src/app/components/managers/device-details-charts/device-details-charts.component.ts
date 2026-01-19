371103c891951e1d3c1bb6f48712377f22dd201c
unknown <phungchien30031993@gmail.com>
2026-01-19 16:01:46 +0700

    add loading effect for tab TongHop

--- src/app/components/managers/device-details-charts/device-details-charts.component.ts
+++ src/app/components/managers/device-details-charts/device-details-charts.component.ts
@@ -4,7 +4,7 @@ import { NgxSelectModule } from 'ngx-select-ex';
 import { DashboardService } from '../../../services/managers/dashboard.service';
 import * as echarts from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
-import { LineChart } from 'echarts/charts';
+import { LineChart, BarChart } from 'echarts/charts';
 import {
   TitleComponent,
   TooltipComponent,
@@ -15,9 +15,13 @@ import {
 import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
 import { Devices } from '../../../models/devices';
 import { DevicesService } from '../../../services/managers/devices.service';
+import { forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 echarts.use([
   CanvasRenderer,
   LineChart,
+  BarChart,
   TitleComponent,
   TooltipComponent,
   GraphicComponent,
@@ -28,7 +32,7 @@ echarts.use([
   selector: 'app-device-details-charts',
   templateUrl: './device-details-charts.component.html',
   styleUrls: ['./device-details-charts.component.css'],
-  imports: [NgxSelectModule, FormsModule, NgxEchartsDirective],
+  imports: [NgxSelectModule, FormsModule, NgxEchartsDirective, CommonModule],
   providers: [provideEchartsCore({ echarts })],
 })
 export class DeviceDetailsChartsComponent implements OnInit {
@@ -51,6 +55,7 @@ export class DeviceDetailsChartsComponent implements OnInit {
   ];
   monthOptionValue = 0;
   devices: Devices[] = [];
+  isLoading = false;
 
   @ViewChild('powerRate', { static: false })
   powerRate!: ElementRef;
@@ -65,135 +70,142 @@ export class DeviceDetailsChartsComponent implements OnInit {
   constructor(
     private dashboardService: DashboardService,
     private devicesService: DevicesService
-  ) {}
+  ) { }
 
   ngOnInit() {
+    this.isLoading = true;
     this.devicesService.getAll().subscribe({
       next: (data) => {
         this.devices = data;
-        if (!this.deviceId) {
+        if (!this.deviceId && data.length > 0) {
           this.deviceId = data[0].Id;
         }
         this.loadData();
       },
+      error: () => {
+        this.isLoading = false;
+      }
     });
   }
   loadData() {
-    this.dashboardService
-      .getDetailsEnergyData(
+    if (!this.deviceId) return;
+    this.isLoading = true;
+    forkJoin({
+      powerRateData: this.dashboardService.getDetailsEnergyData(
+        this.yearValue,
+        this.monthOptionValue,
+        this.deviceId
+      ),
+      wasteOutputData: this.dashboardService.getDetailsWasteOutputData(
         this.yearValue,
         this.monthOptionValue,
         this.deviceId
       )
-      .subscribe({
-        next: (powerRateData) => {
-          this.powerRateChartOption = {
-            title: {
-              text: 'Công suất tiêu thụ',
-              left: 'center',
-              top: 10,
-              textStyle: {
-                fontSize: 26,
-                fontWeight: 'bold',
-                color: '#ffffff',
-              },
-            },
-            tooltip: {
-              trigger: 'axis',
-              formatter: (p: any) =>
-                `${p[0].axisValue}<br/>Công suất: ${p[0].data} kWh`,
-            },
-            grid: {
-              left: '10%',
-              right: '10%',
-              bottom: '10%',
-              top: 30,
-              containLabel: true,
-            },
-            xAxis: {
-              type: 'category',
-              data: powerRateData.map((d) => d.XAxisValue),
-              axisLabel: {
-                color: '#ffffff',
-              },
-            },
-            yAxis: {
-              type: 'value',
-              name: 'kWh',
-              axisLabel: {
-                color: '#ffffff',
-              },
-            },
-            series: [
-              {
-                name: 'Công suất tiêu thụ',
-                type: 'bar',
-                data: powerRateData.map((d) => d.YAxisValue),
-                itemStyle: { color: '#32cb37ff' },
-              },
-            ],
+    }).subscribe({
+      next: (result) => {
+        this.powerRateChartOption = {
+          title: {
+            text: 'Công suất tiêu thụ',
+            left: 'center',
+            top: 10,
             textStyle: {
-              fontFamily: 'Open Sans',
+              fontSize: 26,
+              fontWeight: 'bold',
+              color: '#ffffff',
             },
-          };
-        },
-      });
-    this.dashboardService
-      .getDetailsWasteOutputData(
-        this.yearValue,
-        this.monthOptionValue,
-        this.deviceId
-      )
-      .subscribe({
-        next: (wasteOutputData) => {
-          this.wasteOutputChartOption = {
-            title: {
-              text: 'Khí thải',
-              left: 'center',
-              top: 10,
-              textStyle: {
-                fontSize: 26,
-                fontWeight: 'bold',
-                color: '#ffffff',
-              },
+          },
+          tooltip: {
+            trigger: 'axis',
+            formatter: (p: any) =>
+              `${p[0].axisValue}<br/>Công suất: ${p[0].data} kWh`,
+          },
+          grid: {
+            left: '10%',
+            right: '10%',
+            bottom: '10%',
+            top: 60,
+            containLabel: true,
+          },
+          xAxis: {
+            type: 'category',
+            data: result.powerRateData.map((d) => d.XAxisValue),
+            axisLabel: {
+              color: '#ffffff',
             },
-            tooltip: {
-              trigger: 'axis',
+          },
+          yAxis: {
+            type: 'value',
+            name: 'kWh',
+            axisLabel: {
+              color: '#ffffff',
             },
-            grid: {
-              left: '10%',
-              right: '10%',
-              bottom: '10%',
-              top: 30,
-              containLabel: true,
+          },
+          series: [
+            {
+              name: 'Công suất tiêu thụ',
+              type: 'bar',
+              data: result.powerRateData.map((d) => d.YAxisValue),
+              itemStyle: { color: '#32cb37ff' },
             },
-            xAxis: {
-              type: 'category',
-              data: wasteOutputData.map((d) => d.XAxisValue),
-              axisLabel: {
-                color: '#ffffff',
-              },
-            },
-            yAxis: {
-              type: 'value',
-              name: 'ppm',
-              axisLabel: {
-                color: '#ffffff',
-              },
-            },
-            series: [
-              {
-                name: 'Khí thải',
-                type: 'bar',
-                data: wasteOutputData.map((d) => d.YAxisValue),
-                itemStyle: { color: '#c02323ff' },
-              },
-            ],
+          ],
+          textStyle: {
+            fontFamily: 'Open Sans',
+          },
+        };
+
+        this.wasteOutputChartOption = {
+          title: {
+            text: 'Khí thải',
+            left: 'center',
+            top: 10,
             textStyle: {
-              fontFamily: 'Open Sans',
+              fontSize: 26,
+              fontWeight: 'bold',
+              color: '#ffffff',
             },
-          };
-        },
-      });
+          },
+          tooltip: {
+            trigger: 'axis',
+          },
+          grid: {
+            left: '10%',
+            right: '10%',
+            bottom: '10%',
+            top: 60,
+            containLabel: true,
+          },
+          xAxis: {
+            type: 'category',
+            data: result.wasteOutputData.map((d) => d.XAxisValue),
+            axisLabel: {
+              color: '#ffffff',
+            },
+          },
+          yAxis: {
+            type: 'value',
+            name: 'ppm',
+            axisLabel: {
+              color: '#ffffff',
+            },
+          },
+          series: [
+            {
+              name: 'Khí thải',
+              type: 'bar',
+              data: result.wasteOutputData.map((d) => d.YAxisValue),
+              itemStyle: { color: '#c02323ff' },
+            },
+          ],
+          textStyle: {
+            fontFamily: 'Open Sans',
+          },
+        };
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      }
+    });
   }
 }
